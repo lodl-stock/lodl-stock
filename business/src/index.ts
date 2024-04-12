8e3484a5138c9e6acefcbaec22ca6fee6aef1a5e
Maria Teaca <maria.teaca@stud.acs.upb.ro>
2024-04-12 16:38:07 +0300

    Changed file structure to accommodate global schema.prisma file

---
+++ business/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import http from "http";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import prisma from "./prisma_client";
+import seed from "./seed/seeder";
+import { userRoutes } from "./routes";
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/api/users", userRoutes)
+
+prisma.$connect().then(() => {
+    console.log('Connected to MariaDB');
+    // seed();
+    server.listen(PORT, () => {
+        console.log(`listening on *:${PORT}`);
+    });
+});
